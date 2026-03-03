12851788b62e7e3eaa5fcf61ab867f7f6ccad4f6
Krish <krishpatel.techstaunch@gmail.com>
2026-03-03 13:26:58 +0530

    Renamed hot news images and refactor News component to use dynamic rendering

--- src/components/sections/News.tsx
+++ src/components/sections/News.tsx
@@ -1,10 +1,17 @@
 import Image from "next/image";
-import robot1 from "../../../public/hot-news-images/robot1.png";
-import robot2 from "../../../public/hot-news-images/robot2.png";
-import robot3 from "../../../public/hot-news-images/robot3.png";
-import robot4 from "../../../public/hot-news-images/robot4.png";
-import robot5 from "../../../public/hot-news-images/robot5.png";
+import chip from "../../../public/hot-news-images//chip.png";
+import car_robot from "../../../public/hot-news-images/car-robot.png";
+import factory_robot from "../../../public/hot-news-images/factory-robot.png";
+import robot_arm from "../../../public/hot-news-images/robot-arm.png";
+import yaskawa_robot from "../../../public/hot-news-images/yaskawa-robot.png";
 import title from "../../../public/products-images/Title.png";
+const seminars = [
+  { id: 1, image: factory_robot },
+  { id: 2, image: chip },
+  { id: 3, image: robot_arm },
+  { id: 4, image: car_robot },
+  { id: 5, image: yaskawa_robot },
+];
 const News = () => {
   return (
     <section>
@@ -13,76 +20,26 @@ const News = () => {
         <h2 className="text-2xl font-semibold text-gray-700">HOT NEWS</h2>
       </div>
       <div className="flex flex-col gap-5 my-5 justify-evenly items-center lg:gap-0 lg:px-10 lg:flex-row">
-        <div className="flex flex-col ring-2 ring-gray-100 bg-white w-65">
-          <div className="h-60 w-full relative">
-            <Image
-              src={robot1}
-              alt="robot1"
-              fill
-              className="object-cover"
-            ></Image>
+        {seminars.map((item) => (
+          <div
+            key={item.id}
+            className="flex flex-col ring-2 ring-gray-100 bg-white w-65"
+          >
+            <div className="h-60 w-full relative">
+              <Image
+                src={item.image}
+                alt={`robot-${item.id}`}
+                fill
+                className="object-cover"
+              />
+            </div>
+
+            <span className="text-xs text-gray-600">09/01/2022</span>
+            <span className="text-sm font-medium text-gray-600">
+              [Seminor] ROBOT APPLICATION IN INDUSTRIAL AGE 4.0
+            </span>
           </div>
-          <span className="text-xs text-gray-600">09/01/2022</span>
-          <span className="text-sm font-medium text-gray-600">
-            [Seminor] ROBOT APPLICATION IN INDUSTRIAL AGE 4.0
-          </span>
-        </div>
-        <div className="flex flex-col ring-2 ring-gray-100 bg-white w-65">
-          <div className="h-60 w-full relative">
-            <Image
-              src={robot2}
-              alt="robot2"
-              fill
-              className="object-cover"
-            ></Image>
-          </div>
-          <span className="text-xs text-gray-600">09/01/2022</span>
-          <span className="text-sm font-medium text-gray-600">
-            [Seminor] ROBOT APPLICATION IN INDUSTRIAL AGE 4.0
-          </span>
-        </div>
-        <div className="flex flex-col ring-2 ring-gray-100 bg-white w-65">
-          <div className="h-60 w-full relative">
-            <Image
-              src={robot3}
-              alt="robot3"
-              fill
-              className="object-cover"
-            ></Image>
-          </div>
-          <span className="text-xs text-gray-600">09/01/2022</span>
-          <span className="text-sm font-medium text-gray-600">
-            [Seminor] ROBOT APPLICATION IN INDUSTRIAL AGE 4.0
-          </span>
-        </div>
-        <div className="flex flex-col ring-2 ring-gray-100 bg-white w-65">
-          <div className="h-60 w-full relative">
-            <Image
-              src={robot4}
-              alt="robot4"
-              fill
-              className="object-cover"
-            ></Image>
-          </div>
-          <span className="text-xs text-gray-600">09/01/2022</span>
-          <span className="text-sm font-medium text-gray-600">
-            [Seminor] ROBOT APPLICATION IN INDUSTRIAL AGE 4.0
-          </span>
-        </div>
-        <div className="flex flex-col ring-2 ring-gray-100 bg-white w-65">
-          <div className="h-60 w-full relative">
-            <Image
-              src={robot5}
-              alt="robot5"
-              fill
-              className="object-cover"
-            ></Image>
-          </div>
-          <span className="text-xs text-gray-600">09/01/2022</span>
-          <span className="text-sm font-medium text-gray-600">
-            [Seminor] ROBOT APPLICATION IN INDUSTRIAL AGE 4.0
-          </span>
-        </div>
+        ))}
       </div>
       <div className="flex justify-center">
         <button
