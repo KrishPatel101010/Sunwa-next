658f8edf908f6475afd547044a5415294c31050b
Krish <krishpatel.techstaunch@gmail.com>
2026-03-03 13:49:53 +0530

    Renamed founders images and refactor footer component to use dynamic rendering

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,17 +1,55 @@
 import Image from "next/image";
 import Link from "next/link";
-import founder2 from "../../public/footer-images/founders-names-images/Giới thiệu.png";
-import founder5 from "../../public/footer-images/founders-names-images/Liên hệ.png";
-import founder3 from "../../public/footer-images/founders-names-images/Sản phẩm.png";
-import founder4 from "../../public/footer-images/founders-names-images/Tin tức.png";
-import founder1 from "../../public/footer-images/founders-names-images/Trang chủ.png";
-import founder6 from "../../public/footer-images/founders-names-images/tài liệu.png";
+import Giới_thiệu from "../../public/footer-images/founders-names-images/Giới thiệu.png";
+import Liên_hệ from "../../public/footer-images/founders-names-images/Liên hệ.png";
+import Sản_phẩm from "../../public/footer-images/founders-names-images/Sản phẩm.png";
+import Tin_tức from "../../public/footer-images/founders-names-images/Tin tức.png";
+import Trang_chủ from "../../public/footer-images/founders-names-images/Trang chủ.png";
+import tài_liệu from "../../public/footer-images/founders-names-images/tài liệu.png";
 import facebook from "../../public/footer-images/socials-images/Facebook.png";
 import flickr from "../../public/footer-images/socials-images/Flickr.png";
 import twitter from "../../public/footer-images/socials-images/Twitter.png";
 import youtube from "../../public/footer-images/socials-images/Youtube.png";
 import logo from "../../public/navbar-images/header-logo.png";
-// import list from "../../public/footer-images/list-style.png";
+
+const companies = [
+  { name: "Taiyo Cabletec" },
+  { name: "Nichikon" },
+  { name: "Nichikon" },
+  { name: "Stanley" },
+  { name: "Pro-face" },
+  { name: "Pro-face" },
+];
+
+const categories = [
+  "Robot cables",
+  "Equipment cables",
+  "General-purpose cables",
+  "Others",
+];
+
+const policies = [
+  { label: "Privacy Policy", href: "/" },
+  { label: "Warranty Policy", href: "/" },
+  { label: "Shipping / Installation Policy", href: "/" },
+  { label: "Return / Refund Policy", href: "/" },
+];
+
+const socialLinks = [
+  { src: youtube, alt: "youtube-logo", href: "/" },
+  { src: facebook, alt: "facebook-logo", href: "/" },
+  { src: flickr, alt: "flickr-logo", href: "/" },
+  { src: twitter, alt: "twitter-logo", href: "/" },
+];
+
+const founders = [
+  { src: Trang_chủ, alt: "Trang chủ" },
+  { src: Giới_thiệu, alt: "Giới thiệu" },
+  { src: Sản_phẩm, alt: "Sản phẩm" },
+  { src: Tin_tức, alt: "Tin tức" },
+  { src: Liên_hệ, alt: "Liên hệ" },
+  { src: tài_liệu, alt: "tài liệu" },
+];
 
 const Footer = () => {
   return (
@@ -49,60 +87,17 @@ const Footer = () => {
             PRODUCTS
           </span>
           <div className="grid grid-cols-3 gap-2 mt-3 grid-rows-2">
-            <ul className="flex flex-col list-disc">
-              <li className="font-bold">Taiyo Cabletec</li>
-              <div className="flex flex-col text-gray-400">
-                <span>Robot cables</span>
-                <span>Equipment cables</span>
-                <span>General-purpose cables</span>
-                <span>Others</span>
-              </div>
-            </ul>
-            <ul className="flex flex-col list-disc">
-              <li className="font-bold">Nichikon</li>
-              <div className="flex flex-col text-gray-400">
-                <span>Robot cables</span>
-                <span>Equipment cables</span>
-                <span>General-purpose cables</span>
-                <span>Others</span>
-              </div>
-            </ul>
-            <ul className="flex flex-col list-disc">
-              <li className="font-bold">Nichikon</li>
-              <div className="flex flex-col text-gray-400">
-                <span>Robot cables</span>
-                <span>Equipment cables</span>
-                <span>General-purpose cables</span>
-                <span>Others</span>
-              </div>
-            </ul>
-            <ul className="flex flex-col list-disc">
-              <li className="font-bold">Stanley</li>
-              <div className="flex flex-col text-gray-400">
-                <span>Robot cables</span>
-                <span>Equipment cables</span>
-                <span>General-purpose cables</span>
-                <span>Others</span>
-              </div>
-            </ul>
-            <ul className="flex flex-col list-disc">
-              <li className="font-bold">Pro-face</li>
-              <div className="flex flex-col text-gray-400">
-                <span>Robot cables</span>
-                <span>Equipment cables</span>
-                <span>General-purpose cables</span>
-                <span>Others</span>
-              </div>
-            </ul>
-            <ul className="flex flex-col list-disc">
-              <li className="font-bold">Pro-face</li>
-              <div className="flex flex-col text-gray-400">
-                <span>Robot cables</span>
-                <span>Equipment cables</span>
-                <span>General-purpose cables</span>
-                <span>Others</span>
-              </div>
-            </ul>
+            {companies.map((company, index) => (
+              <ul key={index} className="flex flex-col list-disc pl-5">
+                <li className="font-bold">{company.name}</li>
+
+                {categories.map((item, idx) => (
+                  <li key={idx} className="text-gray-400 list-none ml-4">
+                    {item}
+                  </li>
+                ))}
+              </ul>
+            ))}
           </div>
         </div>
         <div className="flex flex-col flex-1">
@@ -110,25 +105,19 @@ const Footer = () => {
             POLICY
           </span>
           <div className="flex flex-1 justify-between flex-col">
-            <div className="flex flex-col gap-2 mt-3 text-gray-400 ">
-              <Link href={"/"}>Privacy Policy</Link>
-              <Link href={"/"}>Warranty Policy</Link>
-              <Link href={"/"}>Shipping / Installation Policy</Link>
-              <Link href={"/"}>Return / Refund Policy</Link>
+            <div className="flex flex-col gap-2 mt-3 text-gray-400">
+              {policies.map((policy, index) => (
+                <Link key={index} href={policy.href}>
+                  {policy.label}
+                </Link>
+              ))}
             </div>
             <div className="flex gap-2 pb-5">
-              <Link href={"/"}>
-                <Image src={youtube} alt="youtube-logo" />
-              </Link>
-              <Link href={"/"}>
-                <Image src={facebook} alt="facebook-logo" />
-              </Link>
-              <Link href={"/"}>
-                <Image src={flickr} alt="flickr-logo" />
-              </Link>
-              <Link href={"/"}>
-                <Image src={twitter} alt="twitter-logo" />
-              </Link>
+              {socialLinks.map((social) => (
+                <Link key={social.alt} href={social.href}>
+                  <Image src={social.src} alt={social.alt} />
+                </Link>
+              ))}
             </div>
           </div>
         </div>
@@ -138,29 +127,11 @@ const Footer = () => {
           <span>© Copyright 2022 - Sunwa.com.vn - Designed by Webico</span>
         </div>
         <div className="lg:flex hidden gap-7">
-          <div>
-            <Image src={founder1} alt="fouder1"></Image>
-          </div>
-
-          <div>
-            <Image src={founder2} alt="fouder2"></Image>
-          </div>
-
-          <div>
-            <Image src={founder3} alt="fouder3"></Image>
-          </div>
-
-          <div>
-            <Image src={founder4} alt="fouder4"></Image>
-          </div>
-
-          <div>
-            <Image src={founder5} alt="fouder5"></Image>
-          </div>
-
-          <div>
-            <Image src={founder6} alt="fouder6"></Image>
-          </div>
+          {founders.map((founder) => (
+            <div key={founder.alt}>
+              <Image src={founder.src} alt={founder.alt} />
+            </div>
+          ))}
         </div>
       </div>
     </footer>
