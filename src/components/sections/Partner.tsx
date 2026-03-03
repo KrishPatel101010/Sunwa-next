d2844c4af2dc7c0181c968090621cab7a4ca2db1
Krish <krishpatel.techstaunch@gmail.com>
2026-03-03 13:39:14 +0530

    Renamed partners images and refactor Partner component to use dynamic rendering

--- src/components/sections/Partner.tsx
+++ src/components/sections/Partner.tsx
@@ -1,14 +1,25 @@
 import Image from "next/image";
 import title1 from "../../../public/partners-images/Title1.png";
-import partner1 from "../../../public/partners-images/partner1.png";
-import partner2 from "../../../public/partners-images/partner2.png";
-import partner3 from "../../../public/partners-images/partner3.png";
-import partner4 from "../../../public/partners-images/partner4.png";
-import partner5 from "../../../public/partners-images/partner5.jpg";
-import partner6 from "../../../public/partners-images/partner6.png";
-import partner7 from "../../../public/partners-images/partner7.png";
-import partner8 from "../../../public/partners-images/partner8.png";
-import partner9 from "../../../public/partners-images/partner9.png";
+import nato from "../../../public/partners-images/nato.png";
+import omron from "../../../public/partners-images/omron.png";
+import nichicon from "../../../public/partners-images/nichicon.png";
+import stanley from "../../../public/partners-images/stanley.png";
+import koganei from "../../../public/partners-images/koganei.jpg";
+import kel from "../../../public/partners-images/kel.png";
+import mitsumi from "../../../public/partners-images/mitsumi.png";
+
+const partners = [
+  { src: nato, alt: "nato", grayscale: true },
+  { src: omron, alt: "omron", grayscale: true },
+  { src: nichicon, alt: "nichicon", grayscale: true },
+  { src: stanley, alt: "stanley", grayscale: true },
+  { src: koganei, alt: "koganei", grayscale: false, large: true },
+  { src: stanley, alt: "stanley", grayscale: true },
+  { src: kel, alt: "kel", grayscale: true },
+  { src: nichicon, alt: "nichicon", grayscale: true },
+  { src: mitsumi, alt: "mitsumi", grayscale: true },
+];
+
 const Partner = () => {
   return (
     <section>
@@ -18,69 +29,22 @@ const Partner = () => {
         <Image src={title1} alt="design" className="hidden lg:inline"></Image>
       </div>
       <div className="flex flex-col justify-center lg:w-auto lg:flex-row items-center border border-gray-200 gap-5 mx-10 px-5 relative py-10 lg:py-20">
-        <div className="flex-1">
-          <Image
-            className="grayscale w-50 h-auto object-contain"
-            src={partner1}
-            alt="partner1"
-          ></Image>
-        </div>
-        <div className="flex-1">
-          <Image
-            className="grayscale w-50 h-auto object-contain"
-            src={partner2}
-            alt="partner2"
-          ></Image>
-        </div>
-        <div className="flex-1">
-          <Image
-            className="grayscale w-50 h-auto object-contain"
-            src={partner3}
-            alt="partner3"
-          ></Image>
-        </div>
-        <div className="flex-1">
-          <Image
-            className="grayscale w-50 object-contain"
-            src={partner4}
-            alt="partner4"
-          ></Image>
-        </div>
-        <div className="flex-2 relative">
-          <Image
-            className="object-contain w-md lg:w-full"
-            src={partner5}
-            alt="partner5"
-          ></Image>
-        </div>
-        <div className="flex-1">
-          <Image
-            className="grayscale w-50 object-contain"
-            src={partner6}
-            alt="partner6"
-          ></Image>
-        </div>
-        <div className="flex-1">
-          <Image
-            className="grayscale w-50 object-contain"
-            src={partner7}
-            alt="partner7"
-          ></Image>
-        </div>
-        <div className="flex-1">
-          <Image
-            className="grayscale w-50 object-contain"
-            src={partner8}
-            alt="partner8"
-          ></Image>
-        </div>
-        <div className="flex-1">
-          <Image
-            className="grayscale w-50 object-contain"
-            src={partner9}
-            alt="partner9"
-          ></Image>
-        </div>
+        {partners.map((partner, index) => (
+          <div
+            key={index}
+            className={partner.large ? "flex-2 relative" : "flex-1"}
+          >
+            <Image
+              src={partner.src}
+              alt={partner.alt}
+              className={`
+          object-contain
+          ${partner.grayscale ? "grayscale w-50" : ""}
+          ${partner.large ? "w-md lg:w-full" : ""}
+        `}
+            />
+          </div>
+        ))}
       </div>
     </section>
   );
